0d58909cad611191fc786d9655163394da01b409
pengel13 <pedrototiengel@gmail.com>
2023-12-08 15:02:30 -0300

    🔥 delete projeto errado

--- agendaProjectErrado/contact/views/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-from .contact_views import *
-from .contact_forms import *
-from .user_forms import *
-print('pasta das views importada')
-# agora se pode usar tudo que está dentro de contact_views como:
-# from .views import index
-# é como se a pasta views fosse um arquivo views
-# só importar tudo de todos arquivos que nem foi feito com contact_views
